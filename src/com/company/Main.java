870f72462bc43c043c46156c2301ba7ca0e76432
s218156 <218156@edu.p.lodz.pl>
2020-11-08 18:59:16 +0100

    added newFun newFun removed from Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,6 @@ public class Main {
         String nazwaGrupy = "Grupa P";
         String githubID = "Patryk-Korczak";
         System.out.println("Nazwa grupy: " + nazwaGrupy + "\nGithub ID Team Leadera: " + githubID);
-        newFun();
     }
 
     public void newFun(){
