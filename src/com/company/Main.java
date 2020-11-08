ac850472d362ead3d60ed7800dac826a24a06535
s218156 <218156@edu.p.lodz.pl>
2020-11-08 18:55:08 +0100

    added newFun

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,12 @@ public class Main {
     public static void main(String[] args) {
         String nazwaGrupy = "Grupa P";
         String githubID = "Patryk-Korczak";
-        String MyGithubID = "s218156";
         System.out.println("Nazwa grupy: " + nazwaGrupy + "\nGithub ID Team Leadera: " + githubID);
-        System.out.println("Github ID: "+MygithubID);
+        newFun();
+    }
+
+    public void newFun(){
+        String MyGithubID="s218156";
+        System.out.println("My Github ID: "+MyGithubID);
     }
 }
