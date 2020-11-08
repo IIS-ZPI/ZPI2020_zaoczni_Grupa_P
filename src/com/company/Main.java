c4cf692f74cfb8e786fb84fed26251f617817dae
s218156 <218156@edu.p.lodz.pl>
2020-11-08 18:49:47 +0100

    added function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         String nazwaGrupy = "Grupa P";
         String githubID = "Patryk-Korczak";
+        String MyGithubID = "s218156";
         System.out.println("Nazwa grupy: " + nazwaGrupy + "\nGithub ID Team Leadera: " + githubID);
+        System.out.println("Github ID: "+MygithubID);
     }
 }
