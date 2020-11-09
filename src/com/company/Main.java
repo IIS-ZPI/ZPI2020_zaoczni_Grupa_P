be8abb4e348da25a298c06599492fada958ae4c0
Patryk <patrykkorczak7@gmail.com>
2020-11-09 12:08:41 +0100

    cleanup code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,17 @@
-
-
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
         String nazwaGrupy = "Grupa P";
         String githubID = "Patryk-Korczak";
         System.out.println("Nazwa grupy: " + nazwaGrupy + "\nGithub ID Team Leadera: " + githubID);
     }
 
-    public void displayMarcin(){
+    public void displayMarcin() {
         System.out.println("Marcin-Stan");
     }
 
-    public void displayMichal(){
+    public void displayMichal() {
         String MyGithubID="s218156";
         System.out.println("My Github ID: "+MyGithubID);
     }
@@ -22,10 +19,9 @@ public class Main {
     public void displayPatryk() {
         String githubID = "Patryk-Korczak";
         System.out.println("GitHub ID: " + githubID);
-
     }
   
-  public void displayLukasz(){
+    public void displayLukasz() {
         System.out.println("lukasz12121");
     }
 }
