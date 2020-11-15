c87db5f9d836d80e5cde39b14d829af54479fefa
Michał Matusiak <60293254+s218156@users.noreply.github.com>
2020-11-15 18:19:56 +0100

    Update IArithmeticDiv.java

--- src/com/company/IArithmeticDiv.java
+++ src/com/company/IArithmeticDiv.java
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 interface IArithmeticDiv{
     doule Division(double A, doduble B);
 
-=======
-interface IArithmeticDiv{
-    doule Division(double A, doduble B);
-
->>>>>>> origin/Division
 }
